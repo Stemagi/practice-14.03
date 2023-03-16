1a8a2303c05e87e94778d11544f8f165bee42acc
stm <stm2485@gmail.com>
2023-03-16 12:26:59 +0200

    added arrays

--- src/app.jsx
+++ src/app.jsx
@@ -1,34 +1,15 @@
 import React from "react";
 import Jokes from "./jokes";
-export default function(){
+import jokesData from "./jokesData";
+export default function App(){
+  const jokeElements = jokesData.map(joke =>{
+    return <Jokes setup={joke.setup} punchline={joke.punchline}/>
+  })
   return(
     <div>
-      <h1>Funny jokes))</h1>
-      <hr />
-      <Jokes 
-      joke="I got my daughter a fridge for her birthday."
-      punchline="I can't wait to see her face light up when she opens it."/>
-      {/* Ххппхпхп ладно, эта неплохая */}
-      <Jokes 
-      joke="How did the hacker escape the police?"
-      punchline="He just ransomware!" 
-       />
-      <Jokes 
-      joke="Why don't pirates travel on mountain roads?" 
-      punchline="Scurvy."
-      />
-      <Jokes 
-      joke="Why do bees stay in the hive in the winter?" 
-      punchline="Swarm."
-      />
-      <Jokes 
-      joke="What's the best thing about Switzerland?" 
-      punchline="I don't know, but the flag is a big plus!" 
-      />
-      <Jokes 
-      punchline="It’s hard to explain puns to kleptomaniacs because 
-      they always take things literally."
-      />
+      {jokeElements}
     </div>
   )
 }
+
+
--- src/jokes.jsx
+++ src/jokes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export default function Jokes(props){
   return(
   <div>
-    {props.joke &&  <h3>Setup {props.joke}</h3>}
+    {props.setup &&  <h3>Setup {props.setup}</h3>}
     <p>Punchline {props.punchline}</p>
     <hr />
   </div>
